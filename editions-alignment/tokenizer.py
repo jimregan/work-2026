422e809fd3bdfa622d54150e977de07105eba798
Jim O'Regan <joregan@kth.se>
2026-04-20 13:58:35 +0200

    rm

--- editions-alignment/tokenizer.py
+++
@@ -1,7 +0,0 @@
-from sacremoses import MosesSentenceSplitter
-
-
-def split_sentences(text: str, lang: str = "en") -> list[str]:
-    splitter = MosesSentenceSplitter(lang)
-    sentences = splitter.split(text.splitlines())
-    return [s.strip() for s in sentences if s.strip()]

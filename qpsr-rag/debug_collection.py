89b442ef34f7501a14b377eae2c77291945a5635
Jim O'Regan <joregan@kth.se>
2026-09-24 21:14:13 +0200

    tidy

--- qpsr-rag/debug_collection.py
+++
@@ -1,5 +0,0 @@
-import chromadb
-
-client = chromadb.PersistentClient(path="/data/chroma_db")
-print("collections:", client.list_collections())
-print("count:", client.get_collection("stl-qpsr").count())
